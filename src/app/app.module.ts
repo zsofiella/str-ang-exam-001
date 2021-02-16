eec07adf490e88c758665ec675d41c0f64b0dd33
Zsofi <galambvarizsofia@gmail.com>
2021-02-16 09:29:37 +0100

    navbar created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './src/app/common/navigation/navigation.component';
+import { HomeComponent } from './common/home/home.component';
+import { HeroesComponent } from './common/heroes/heroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    HomeComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
